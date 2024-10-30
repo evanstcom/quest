2d7bfa12ec12d3a0253561853aeb65d0f96e5f06
evanstcom <zhenyakoks@gmail.com>
2024-10-30 12:22:22 +0300

    swiper add

--- main.js
+++ main.js
@@ -5,17 +5,29 @@ import Swiper from "swiper/bundle";
 // import styles bundle
 import "swiper/css/bundle";
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  cssWidthAndHeight: true,
-  slidesPerView: "auto",
-  visibilityFullFit: true,
-  autoResize: false,
-  loopedSlides: 4,
-  loop: true,
-  spaceBetween: 24,
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    hide: false,
-  },
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
 });
